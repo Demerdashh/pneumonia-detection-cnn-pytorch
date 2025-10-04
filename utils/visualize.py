74aed70c013a2de05e1ed9ecc00a43a2a0daca2d
Youssef El Demerdash <youssefeldemerdashh@gmail.com>
2025-10-05 00:11:26 +0300

    Create visualize.py

---
+++ utils/visualize.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, roc_curve, auc
+import seaborn as sns
+
+def plot_confusion_matrix(y_true, y_pred, labels=["NORMAL", "PNEUMONIA"]):
+    cm = confusion_matrix(y_true, y_pred)
+    plt.figure(figsize=(5,4))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+                xticklabels=labels, yticklabels=labels)
+    plt.xlabel("Predicted")
+    plt.ylabel("True")
+    plt.title("Confusion Matrix")
+    plt.show()
+
+def plot_roc_curve(y_true, y_scores):
+    fpr, tpr, _ = roc_curve(y_true, y_scores)
+    roc_auc = auc(fpr, tpr)
+    plt.plot(fpr, tpr, label=f"AUC = {roc_auc:.3f}")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("ROC Curve")
+    plt.legend()
+    plt.grid()
+    plt.show()
