b628a5709b22f6168ee4202f379135375b751dba
Youssef El Demerdash <youssefeldemerdashh@gmail.com>
2025-10-05 00:10:38 +0300

    Create train_eval.py

---
+++ utils/train_eval.py
@@ -0,0 +1,42 @@
+import torch
+from tqdm import tqdm
+
+def train_one_epoch(model, loader, criterion, optimizer, device):
+    model.train()
+    total_loss, correct, total = 0, 0, 0
+
+    for images, labels in tqdm(loader, desc="Training", leave=False):
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+        preds = outputs.argmax(dim=1)
+        correct += (preds == labels).sum().item()
+        total += labels.size(0)
+
+    return total_loss / len(loader), correct / total
+
+
+def evaluate(model, loader, criterion, device):
+    model.eval()
+    total_loss, correct, total = 0, 0, 0
+    all_labels, all_preds = [], []
+
+    with torch.no_grad():
+        for images, labels in tqdm(loader, desc="Evaluating", leave=False):
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            total_loss += loss.item()
+            preds = outputs.argmax(dim=1)
+            correct += (preds == labels).sum().item()
+            total += labels.size(0)
+            all_labels.extend(labels.cpu().numpy())
+            all_preds.extend(preds.cpu().numpy())
+
+    acc = correct / total
+    return total_loss / len(loader), acc, all_labels, all_preds
